4c821cb8d1c0be64d744ef0d68cac89eaf527859
Trevor <valentinosolischang@gmail.com>
2025-12-09 18:18:32 -0500

    Add GetAllAlertsQuery record for retrieving all alerts

---
+++ src/main/java/com/spire/platform/u20231b475/monitoring/domain/model/queries/GetAllAlertsQuery.java
@@ -0,0 +1,10 @@
+package com.spire.platform.u20231b475.monitoring.domain.model.queries;
+
+/**
+ * Query to retrieve all available alerts.
+ * 
+ * @author Valentino Solis
+ */
+public record GetAllAlertsQuery() {
+
+}
