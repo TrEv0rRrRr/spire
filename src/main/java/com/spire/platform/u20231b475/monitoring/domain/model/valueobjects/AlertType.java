56d4e762063bccabed9f8d32b61b8c9372ff7c4a
Trevor <valentinosolischang@gmail.com>
2025-12-09 18:18:37 -0500

    Add AlertType enum and SatelliteCode value object for alert management

---
+++ src/main/java/com/spire/platform/u20231b475/monitoring/domain/model/valueobjects/AlertType.java
@@ -0,0 +1,11 @@
+package com.spire.platform.u20231b475.monitoring.domain.model.valueobjects;
+
+/**
+ * This enum represents the possible alert types
+ */
+public enum AlertType {
+  UNSAFE_ORBIT_TASK,
+  NODE_COMMUNICATION_LOST,
+  SYSTEM_ERROR,
+  OTHER
+}
