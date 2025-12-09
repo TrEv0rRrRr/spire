5808062df04315b3be180b151a18e7766fe0b8ba
Trevor <valentinosolischang@gmail.com>
2025-12-09 17:44:49 -0500

    Enable JPA auditing in U20231b475Application

--- src/main/java/com/spire/platform/u20231b475/U20231b475Application.java
+++ src/main/java/com/spire/platform/u20231b475/U20231b475Application.java
@@ -2,8 +2,10 @@ package com.spire.platform.u20231b475;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class U20231b475Application {
 
 	public static void main(String[] args) {
