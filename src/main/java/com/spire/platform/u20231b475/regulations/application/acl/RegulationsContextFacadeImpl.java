23239054cb89123c756f777c265e0e806c8e551b
Trevor <valentinosolischang@gmail.com>
2025-12-09 16:42:05 -0500

    Enhance RegulationsContextFacadeImpl to include OrbitThresholdRepository and implement isUnderutilized method

--- src/main/java/com/spire/platform/u20231b475/regulations/application/acl/RegulationsContextFacadeImpl.java
+++ src/main/java/com/spire/platform/u20231b475/regulations/application/acl/RegulationsContextFacadeImpl.java
@@ -4,14 +4,22 @@ import org.springframework.stereotype.Service;
 
 import com.spire.platform.u20231b475.regulations.domain.model.queries.GetOrbitThresholdByOrbitClassQuery;
 import com.spire.platform.u20231b475.regulations.domain.service.OrbitThresholdQueryService;
+import com.spire.platform.u20231b475.regulations.infrastructure.persistence.jpa.repositories.OrbitThresholdRepository;
 import com.spire.platform.u20231b475.regulations.interfaces.acl.RegulationsContextFacade;
 
+/**
+ * RegulationsContextFacadeImplementation
+ * 
+ * @author Valentino Solis
+ */
 @Service
 public class RegulationsContextFacadeImpl implements RegulationsContextFacade {
   private final OrbitThresholdQueryService service;
+  private final OrbitThresholdRepository repo;
 
-  public RegulationsContextFacadeImpl(OrbitThresholdQueryService service) {
+  public RegulationsContextFacadeImpl(OrbitThresholdQueryService service, OrbitThresholdRepository repo) {
     this.service = service;
+    this.repo = repo;
   }
 
   @Override
@@ -21,5 +29,11 @@ public class RegulationsContextFacadeImpl implements RegulationsContextFacade {
     var orbitThreshold = service.handle(query);
 
     return orbitThreshold.isEmpty() ? 0L : orbitThreshold.get().getId();
+  }
+
+  public boolean isUnderutilized(String orbitClass, Integer estimatedDuration) {
+    return repo.findByOrbitClass(orbitClass)
+        .map(threshold -> estimatedDuration < (threshold.getMaxSafeDuration() * 0.2))
+        .orElse(false);
   }
 }
