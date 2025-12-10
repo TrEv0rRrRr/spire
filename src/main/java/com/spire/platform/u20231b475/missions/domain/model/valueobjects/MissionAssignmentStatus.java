3a6de898e56b0f12ebbe901180463998782cde74
Trevor <valentinosolischang@gmail.com>
2025-12-09 19:17:56 -0500

    Add MissionAssignmentStatus enum for mission assignment status representation

---
+++ src/main/java/com/spire/platform/u20231b475/missions/domain/model/valueobjects/MissionAssignmentStatus.java
@@ -0,0 +1,9 @@
+package com.spire.platform.u20231b475.missions.domain.model.valueobjects;
+
+/**
+ * This enum represents the possible statuses for the mission assignment
+ */
+public enum MissionAssignmentStatus {
+  SCHEDULED,
+  REJECTED
+}
