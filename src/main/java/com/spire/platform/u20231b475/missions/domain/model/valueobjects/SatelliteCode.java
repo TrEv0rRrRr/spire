65c53cb84068dd7e7a804a903466790d89196c21
Trevor <valentinosolischang@gmail.com>
2025-12-09 19:17:59 -0500

    Add SatelliteCode value object for satellite code representation and validation

---
+++ src/main/java/com/spire/platform/u20231b475/missions/domain/model/valueobjects/SatelliteCode.java
@@ -0,0 +1,20 @@
+package com.spire.platform.u20231b475.missions.domain.model.valueobjects;
+
+import java.util.UUID;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * This VO represents the satellite code, it's the business unique identifier
+ */
+@Embeddable
+public record SatelliteCode(@NotBlank(message = "The satellite code is required") String satelliteCode) {
+  public SatelliteCode {
+    try {
+      UUID.fromString(satelliteCode);
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException("Satellite code must be a valid UUID", e);
+    }
+  }
+}
