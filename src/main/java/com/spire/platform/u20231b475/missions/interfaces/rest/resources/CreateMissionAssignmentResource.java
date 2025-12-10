054511a823e40cafe22ac57557a0e032e78f8e68
Trevor <valentinosolischang@gmail.com>
2025-12-09 19:57:12 -0500

    Add CreateMissionAssignmentResource for mission assignment creation

---
+++ src/main/java/com/spire/platform/u20231b475/missions/interfaces/rest/resources/CreateMissionAssignmentResource.java
@@ -0,0 +1,19 @@
+package com.spire.platform.u20231b475.missions.interfaces.rest.resources;
+
+import java.time.LocalDateTime;
+
+import com.spire.platform.u20231b475.missions.domain.model.valueobjects.MissionAssignmentStatus;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+/**
+ * Create mission assignment resource
+ */
+public record CreateMissionAssignmentResource(@NotBlank(message = "Satellite code is required") String satelliteCode,
+    @NotBlank(message = "Orbit class is required") String orbitClass,
+    @NotNull(message = "The estimated duration is required") Integer estimatedDuration, MissionAssignmentStatus status,
+    @NotNull(message = "The requested date is required") @PastOrPresent LocalDateTime requestedAt) {
+
+}
