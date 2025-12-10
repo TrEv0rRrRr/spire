5811a3ef8388aec636b0106676b4983edeab164a
Trevor <valentinosolischang@gmail.com>
2025-12-09 20:04:05 -0500

    Refactor CreateMissionAssignmentResource to include validation for estimated duration

--- src/main/java/com/spire/platform/u20231b475/missions/interfaces/rest/resources/CreateMissionAssignmentResource.java
+++ src/main/java/com/spire/platform/u20231b475/missions/interfaces/rest/resources/CreateMissionAssignmentResource.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.spire.platform.u20231b475.missions.domain.model.valueobjects.MissionAssignmentStatus;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
@@ -12,8 +13,9 @@ import jakarta.validation.constraints.PastOrPresent;
  * Create mission assignment resource
  */
 public record CreateMissionAssignmentResource(@NotBlank(message = "Satellite code is required") String satelliteCode,
-    @NotBlank(message = "Orbit class is required") String orbitClass,
-    @NotNull(message = "The estimated duration is required") Integer estimatedDuration, MissionAssignmentStatus status,
-    @NotNull(message = "The requested date is required") @PastOrPresent LocalDateTime requestedAt) {
+        @NotBlank(message = "Orbit class is required") String orbitClass,
+        @NotNull(message = "The estimated duration is required") @Min(1) Integer estimatedDuration,
+        MissionAssignmentStatus status,
+        @NotNull(message = "The requested date is required") @PastOrPresent LocalDateTime requestedAt) {
 
 }
